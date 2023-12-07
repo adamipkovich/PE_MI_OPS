921c089a839f7b508750d7f6ea8969735b0986b8
Adam Ipkovich <71510025+adamipkovich@users.noreply.github.com>
2023-12-07 15:04:04 +0100

    sklearn pipeline dvc projektként

--- train.py
+++ train.py
@@ -3,12 +3,12 @@ import sklearn
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+from sklearn.pipeline import Pipeline
 from sklearn.tree import plot_tree
 import mlflow
 
 
 if __name__ == "__main__":
-
 
     ## hova mentse a modelt
     mlflow.set_tracking_uri("http://localhost:5000") ## ez egy lokális server,
@@ -21,16 +21,23 @@ if __name__ == "__main__":
 
     ## modellezés - Valami egyszerű legyen egy döntési fa
     with mlflow.start_run() as run:
+
         #tanítás
-        clf = DecisionTreeClassifier()
-        clf.fit(X, y)
+
+        pipe = Pipeline([
+            ("minmax", sklearn.preprocessing.MinMaxScaler()),
+            ## Bármilyen lépés, pl dimenzió redukció, feature engineering, stb
+            ("clf", DecisionTreeClassifier())
+        ])
+
+        pipe.fit(X, y)
 
         ## validálás
-        pred = clf.predict(X)
+        pred = pipe.predict(X)
         accuracy = accuracy_score(y, pred)
         cm = confusion_matrix(y, pred, normalize='true')
         tn, fp, fn, tp = cm.ravel()
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=clf.classes_)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
         disp.plot()
 
 
@@ -39,6 +46,8 @@ if __name__ == "__main__":
         mlflow.log_metric("fp", fp)
         mlflow.log_metric("fn", fn)
         mlflow.log_metric("tp", tp)
+
+
 
 
         # mlflow.sklearn.log_model(clf, "decision_tree_model")
